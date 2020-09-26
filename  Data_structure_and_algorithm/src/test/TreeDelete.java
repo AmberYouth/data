368d9329fddb1d2d2df02d43cbdcc4694295997e
yangshijie <2374575227@qq.com>
2020-09-26 18:32:40 +0800

    update

---  Data_structure_and_algorithm/src/test/TreeDelete.java
+++  Data_structure_and_algorithm/src/test/TreeDelete.java
@@ -1,5 +1,9 @@
 package test;
 
 public class TreeDelete {
-
+    public static void main(String[] args) {
+        long i = 32768;
+        int j = (int)i;
+        System.out.println(j);
+    }
 }
---
+++  Data_structure_and_algorithm/src/tree/heapSort.java
@@ -0,0 +1,24 @@
+package tree;
+
+public class heapSort {
+    public static void main(String[] args) {
+        int arr[] = {4,6,8,5,9};
+
+    }
+
+    public static void heapSort(int[] arr){
+        System.out.println("堆排序！！");
+    }
+
+    //将一个数组（二叉树），调整成一个大顶堆
+
+    /**
+     *功能：完成将以i对应的非叶子结点的树调整成大顶堆
+     * @param arr 待调整的数组
+     * @param i 表示非叶子结点再数组中的索引
+     * @param length 表示对多少个数组进行调整,length再逐渐减少
+     */
+    public static void adjustHeap(int[] arr,int i, int length){
+
+    }
+}
